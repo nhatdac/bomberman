ec12ab7572193d291de8714e95cd25d519abe8eb
Nhat Dac <nhatdac22@gmail.com>
2025-02-11 16:38:17 +0700

    update enemies
    (cherry picked from commit 2f50c2ed51352ed010823d84175014c65c4b5e40)

--- core/src/main/java/thaydac/com/enemies/Enemy4.java
+++ core/src/main/java/thaydac/com/enemies/Enemy4.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import thaydac.com.Master;
 import thaydac.com.MyActor;
+import thaydac.com.Utils;
 
 // Balloon
 public class Enemy4 extends EnemyActor {
@@ -14,6 +15,7 @@ public class Enemy4 extends EnemyActor {
         super(x, y, s);
         Texture texture = new Texture("enemy4.png");
         setAnimation(texture, 11, 1, 0.3f);
+        type = Utils.ENEMY_TYPE4;
         speed = 1;
         speedX = speed;
     }
--- core/src/main/java/thaydac/com/enemies/Enemy5.java
+++ core/src/main/java/thaydac/com/enemies/Enemy5.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import thaydac.com.Master;
 import thaydac.com.MyActor;
+import thaydac.com.Utils;
 
 // Balloon
 public class Enemy5 extends EnemyActor {
@@ -14,6 +15,7 @@ public class Enemy5 extends EnemyActor {
         super(x, y, s);
         Texture texture = new Texture("enemy5.png");
         setAnimation(texture, 11, 1, 0.3f);
+        type = Utils.ENEMY_TYPE5;
         speed = 1;
         speedX = speed;
     }
